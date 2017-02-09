595fd1f802f2a383ccdb37178085b5fe72fcd09f
Konnor Petersen <konnorjp@gmail.com>
2017-02-08 23:34:36 -0700

    3 second after complete, wiki information appears

--- methods.js
+++ methods.js
@@ -7,7 +7,7 @@ var firstY = 0;
 //   ERASE:2,
 // };
 
-var wikiURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=";
+//var wikiURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=";
 var clickCount = 0;
 
 function onCanvasClick(canvas, event) {
@@ -35,24 +35,35 @@ function onCanvasClick(canvas, event) {
 function onComplete(event) {
   document.getElementById("connect_container").style.display = "none";
   document.getElementById("gif_container").style.display = "block";
-  // testApi(event);
+  testApi(event);
 }
 
+
 function testApi(e) {
-  var request = wikiURL + "My%20Little%20Pony";
+  var request = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=My_Little_Pony&callback=?"; //wikiURL + "My%20Little%20Pony";
   e.preventDefault();
   $.ajax({
     url : request,
     dataType : "json",
     success : function(parsed_json) {
-      var group = parsed_json["query"]["pages"];
-      var allPropertyNames = Object.keys(group);
-      var name = allPropertyNames[0];
-      var value = group[name];
-
+     // var group = parsed_json["query"]["pages"];
+     // var allPropertyNames = Object.keys(group);
+     // var name = allPropertyNames[0];
+     // var value = group[name];
+      var ponytalk = parsed_json["query"]["pages"]["490081"]["extract"];
+      //console.log(ponytalk);
       var everything = "<h2>Wiki article on My Little Pony</h2>";
-      everything += "<p>" + value + "</p>";
-      document.getElementById("wiki_info").innerHTML=everything;
+      console.log("everything");
+      everything += "<p>" + ponytalk/*value*/ + "</p>";
+      setTimeout(function(){
+            document.getElementById("gif_container").style.display="none";
+            document.getElementById("text_here").style.display = "block";
+            document.getElementById("text_here").innerHTML=everything;/*wiki_info*/
+            
+            document.getElementById("text_here").style.margin = "50px 300px";
+            document.getElementsByTagName("h2").style.textAlign = "center";
+            
+                     },3000);
     }
   });
 }
